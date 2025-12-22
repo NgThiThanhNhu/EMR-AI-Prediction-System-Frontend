a344a4ad741ca99540849655f8324add5eebd364
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-12-22 17:19:18 +0700

    ThanhNhu: add config api

---
+++ erm-ai-prediction-system-frontend/src/apis/index.jsx
@@ -0,0 +1,8 @@
+// ví dụ: import { axiosClient } from "./config";
+
+// export const PublisherApi = {
+//     addPublisher: async (data) => {
+//         const response = await axiosClient.post(`/api/Publisher/AddPublisher`, data);
+//         return response.data;
+//     }
+// }
